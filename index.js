4d410087c5e78a6da298094b5ccb5be66e91960f
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-09-27 22:25:11 -0300

    create

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/teste", function (req, res) {
+  res.render("page");
+});
+app.listen(3000);
